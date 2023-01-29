a2eb01fa17c774c66fb245f0806231903dd1458e
Bamigboye Akinbode <bodejuniorforever@gmail.com>
2023-01-29 22:29:09 +0100

    changes

--- arababc/app/helpers.py
+++ arababc/app/helpers.py
@@ -8,28 +8,28 @@ from django.db.models import Q
 class CustomFilters:
 
     def __init__(self):
-        members = Members.objects.all()
-        members_not_children = members.filter(
+        self.members = Members.objects.all()
+        members_not_children = self.members.filter(
             date_of_birth__lt=datetime.now() - relativedelta(years=12)).all()
-        youth = members.filter(
+        youth = self.members.filter(
             date_of_birth__gte=datetime.now() - relativedelta(years=45), date_of_birth__lte=datetime.now() - relativedelta(years=18))
-        teens = members.filter(
+        teens = self.members.filter(
             date_of_birth__gte=datetime.now() - relativedelta(years=13), date_of_birth__lt=datetime.now() - relativedelta(years=18))
-        Single = members.filter(
+        Single = self.members.filter(
             date_of_birth__gte=datetime.now() - relativedelta(years=18), marital_status='single')
-        Married = members.filter(marital_status='married')
-        Widows_Widowers = members.filter(marital_status='widowed')
-        today_birthday_celebrants = members.filter(
+        Married = self.members.filter(marital_status='married')
+        Widows_Widowers = self.members.filter(marital_status='widowed')
+        today_birthday_celebrants = self.members.filter(
             date_of_birth__month=datetime.today().month, date_of_birth__day=datetime.today().day)
-        this_months_celebrants = members.filter(
+        this_months_celebrants = self.members.filter(
             date_of_birth__month=datetime.today().month)
-        WMU = members.filter(Q(marital_status='widowed') | Q(
+        WMU = self.members.filter(Q(marital_status='widowed') | Q(
             marital_status='married'), gender='female')
-        MMU = members.filter(Q(marital_status='widowed') | Q(
+        MMU = self.members.filter(Q(marital_status='widowed') | Q(
             marital_status='married'), gender='male')
 
         self.filters = {
-            'All members': [i for i in members],
+            'All members': [i for i in self.members],
             'All Members minus children': [i for i in members_not_children],
             'Youths': [i for i in youth],
             'Teens': [i for i in teens],
