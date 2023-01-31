544df7e049e7cefb51b808b86f18de2cc3b8b0c2
Bamigboye Akinbode <bodejuniorforever@gmail.com>
2023-01-31 19:53:54 +0100

    removed host_name

--- arababc/production.py
+++ arababc/production.py
@@ -30,7 +30,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # DBHOST is only the server name, not the full URL
-hostname = os.environ['DBHOST']
+# hostname = os.environ['DBHOST']
 
 # Configure Postgres database; the full username for PostgreSQL flexible server is
 # username (not @sever-name).
