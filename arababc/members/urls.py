44fc2387313b988beda66c46f7797cd45fad7828
Bamigboye Akinbode <bodejuniorforever@gmail.com>
2023-01-31 14:58:55 +0100

    {-}

--- arababc/members/urls.py
+++
@@ -1,9 +0,0 @@
-from members.views import *
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('new_member', views.new_member, name='new_member'),
-    path('member_page/<int:id>', views.member_page, name='member_page'),
-    path('datapage', views.data_page, name='datapage'),
-]
--- arababc/messaging/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('send-text', views.send_sms, name='send_sms'),
-    path('get-filters', views.get_filter, name='get_filter'),
-
-]
