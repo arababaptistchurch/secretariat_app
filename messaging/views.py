166c1c7b24de04b8cdb5d5975dab102e26f72f11
Bamigboye Akinbode <bodejuniorforever@gmail.com>
2023-01-31 18:27:45 +0100

    {+}

--- messaging/views.py
+++ messaging/views.py
@@ -24,7 +24,7 @@ def get_filter(request):
 
         sms.sms_sender('Araba Bc', numbers, message)
 
-    return render(request, 'messaging/messaging.html', {"group_list": filters.filters, "sms_balance": sms.balance_getter(), "sms_report": sms.report_getter(), "URI": uri})
+    return render(request, 'messaging/messaging.html', {"group_list": filters.filters, "sms_balance": sms.balance_getter(), "sms_report": sms.report_getter()})
     # return render(request, 'messaging/messaging.html', {"group_list": filters.filters})
 
 
